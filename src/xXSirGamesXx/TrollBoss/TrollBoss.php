d98d932f7a83d5f9309be64ce79c5dc7ac337d1e
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-06 21:05:13 -0400

    Update TrollBoss.php

--- src/xXSirGamesXx/TrollBoss/TrollBoss.php
+++ src/xXSirGamesXx/TrollBoss/TrollBoss.php
@@ -11,20 +11,22 @@ use pocketmine\command\CommandSender;
 
 
 class TrollBoss extends PluginBase implements Listener {
-	
+	const PREFIX = "[TrollBossPE] "
 	public function onLoad() { 
-		$this->getLogger()->info(C::AQUA . "Loading plugin TrollBossPE"); ]
+		$this->getLogger()->info(C::AQUA . "Loading TrollBossPE by xXSirButterXx.."); ]
 	}
 	
 	public function onEnable() {  
-		$this->getLogger()->info(C::AQUA . "TrollBossPE Enabled!"); 
+		$this->getLogger()->info(C::AQUA . "TrollBossPE by xXSirButterXx Enabled!"); 
 	}
 	
 	public function onDisable() {
-		$this->getLogger()->info(C::AQUA . "TrollBossPE disabled"); 
+		$this->getLogger()->info(C::AQUA . "TrollBossPE by xXSirButterXx Disabled!"); 
 	}
 	public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
 		if($cmd->getName() == "trollboss"){
+			if($sender->hasPermissions("troll.cmd")){
+				$sender->sendMessage(self::PREFIX . "Please use /trollboss help <pagre number>");
 		}
 	}
 }
