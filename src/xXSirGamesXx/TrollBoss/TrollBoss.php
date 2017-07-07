a94f4167628e2e82cfa3056dc77956972535ec6b
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-06 21:35:18 -0400

    Update TrollBoss.php

--- src/xXSirGamesXx/TrollBoss/TrollBoss.php
+++ src/xXSirGamesXx/TrollBoss/TrollBoss.php
@@ -11,7 +11,10 @@ use pocketmine\command\CommandSender;
 
 
 class TrollBoss extends PluginBase implements Listener {
-	const PREFIX = "[TrollBossPE] "
+	const PREFIX = "[TrollBossPE] "; //oops typo
+	const HELP_TOPPER = "======[TrollBosPE Help]======";
+	const NO_PERM = "[TrollBossPE] You don't have permission to run this command!";
+		
 	public function onLoad() { 
 		$this->getLogger()->info(C::AQUA . "Loading TrollBossPE by xXSirButterXx.."); ]
 	}
@@ -27,6 +30,24 @@ class TrollBoss extends PluginBase implements Listener {
 		if($cmd->getName() == "trollboss"){
 			if($sender->hasPermissions("troll.cmd")){
 				$sender->sendMessage(self::PREFIX . "Please use /trollboss help <pagre number>");
+			}
+				if($args[0] === "help"){
+					if($sender->hasPermission("troll.cmd") || $sender->hasPermission("troll.cmd.help")){
+						if($args[1] === "1"){
+							$sender->sendMessage(self::HELP_TOPPER);
+							$sender->sendMessage("/trollboss help <help-number>");
+						}
+						elseif($args[1] === "2"){
+						       $sender->sendMessage(self::HELP_TOPPER);
+						}
+						       
+					}else{
+						$sender->sendMessage(self::NO_PERM);
+					}
+						
+				}
+		
+						
 		}
 	}
 }
