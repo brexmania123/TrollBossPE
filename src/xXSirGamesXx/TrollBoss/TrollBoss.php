ac441558c7de392c7920a53edac4b184b592f5d6
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-07 10:13:17 -0400

    Update TrollBoss.php
    Idk if it works yet

--- src/xXSirGamesXx/TrollBoss/TrollBoss.php
+++ src/xXSirGamesXx/TrollBoss/TrollBoss.php
@@ -28,17 +28,30 @@ class TrollBoss extends PluginBase implements Listener {
 	}
 	public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
 		if($cmd->getName() == "trollboss"){
-			if($sender->hasPermission("troll.cmd"){
-				$sender->sendMessage(self::PREFIX . "Use /troll help to see all the commands.");
+			if($sender->hasPermission("troll.cmd")){
+				$sender->sendMessage(self::PREFIX . "Use /troll help for a list of commands");
 			}else{
 				$sender->sendMessage(self::NO_PERM);
 			}
-			   if(isset($args[0])){
-				   switch($args[0]){
-						   case: "help":
-						   break;
-				   }
-			   }	
-		}
-	}
+			if(isset($args[0]){
+				if($sender->hasPermission("troll.cmd") || $sender->hasPermission("troll.cmd.help")){
+					if(strtolower($args[0]) === "help"){
+						$sender->sendMessage(self::HELP_TOPPER);
+						$sender->sendMessage(self::PREFIX . "- /troll help: Shows help pages");
+						if(isset($args[1]){
+							if(strtolower($args[1]) === "1"){
+								$sender->sendMessage(self::HELP_TOPPER);
+								$sender->sendMessage(seld::PREFIX . "- /troll help: Shows help pages");
+							}
+							elseif(strtolower($args[1]) === "2"){
+								$sender->sendMessage(self::HELP_TOPPER);
+							}
+							elseif(strtolower($args[1]) === "3"){
+							}
+						}   
+					}
+				}else{
+					$sender->sendmessage(self::NO_PERM);
+				}
+			}
 }
