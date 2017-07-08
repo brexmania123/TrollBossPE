9e6fb80b7f690faf066646bc973fe3b80b02ee63
xXSirGamesXx <xXSirGamesXx@gmail.com>
2017-07-07 22:18:26 -0400

    Setup for new updates.

--- src/xXSirGamesXx/TrollBoss/TrollBoss.php
+++ src/xXSirGamesXx/TrollBoss/TrollBoss.php
@@ -7,6 +7,7 @@ use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as C;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 
 
 
@@ -28,30 +29,41 @@ class TrollBoss extends PluginBase implements Listener {
 	}
 	public function onCommand(CommandSender $sender,Command $cmd,$label,array $args){
 		if($cmd->getName() == "trollboss"){
-			if($sender->hasPermission("troll.cmd")){
-				$sender->sendMessage(self::PREFIX . "Use /troll help for a list of commands");
-			}else{
-				$sender->sendMessage(self::NO_PERM);
-			}
-			if(isset($args[0]){
-				if($sender->hasPermission("troll.cmd") || $sender->hasPermission("troll.cmd.help")){
-					if(strtolower($args[0]) === "help"){
-						$sender->sendMessage(self::HELP_TOPPER);
-						$sender->sendMessage(self::PREFIX . "- /troll help: Shows help pages");
-						if(isset($args[1]){
-							if(strtolower($args[1]) === "1"){
+			if ($sender instanceof Player) {
+				if (!(isset($args[0]))) {
+					if ($sender->hasPermission("troll.cmd")) {
+						$sender->sendMessage(self::PREFIX . "Please type /troll help.");
+						} else {
+							$sender->sendMessage(self::PREFIX . self::NO_PERM);
+							}
+                    }
+					$arg = array_shift($args);
+                    switch ($arg) {
+						case "help":
+							if($sender->hasPermission("troll.cmd") || $sender->hasPermission("troll.cmd.help")){
 								$sender->sendMessage(self::HELP_TOPPER);
-								$sender->sendMessage(seld::PREFIX . "- /troll help: Shows help pages");
+								$sender->sendMessage("/trollboss help: show help pages");
+								if(isset($args[1])){
+									switch($args[1]){
+										case "1":
+											$sender->sendMessage(self::HELP_TOPPER);
+											$sender->sendMessage("/trollboss help: show help pages");
+										break;
+									}
+								}
+							}else{
+								$sender->sendMessage(self::PREFIX , self::NO_PERM);
 							}
-							elseif(strtolower($args[1]) === "2"){
-								$sender->sendMessage(self::HELP_TOPPER);
-							}
-							elseif(strtolower($args[1]) === "3"){
-							}
-						}   
+						break;
 					}
-				}else{
-					$sender->sendmessage(self::NO_PERM);
-				}
+			}else{
+				$sender->sendMessage(self::PREFIX . "This command only works in-game, nothing personal.");
 			}
+				
+				
+		}
+	}
 }
+
+
+
